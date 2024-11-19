f764c339665a8a5da9da67b57f73c8ee36f5b134
Kurimasu Tanaka <kamyshek185@gmail.com>
2024-11-19 11:10:56 +0200

    Moved project to differen template

---
+++ ZAK.Tests/GeoManagerTests.cs
@@ -0,0 +1,34 @@
+using BlazorApp.DA;
+using BlazorApp.DB;
+using BlazorApp.GeoDataManager;
+using Xunit;
+
+namespace BlazorApp.Tests;
+
+public class GeoDataManagerTests
+{
+
+    [Fact]
+    public async void Proper_Address_Loaded_Coordinates_Received()
+    {
+        // Arrange
+        ICoordinatesProvider _coordinatesProvider = new NominatimCoordinatesProvider();
+
+        
+        var address = new Address
+        {
+            streetName = "Київ проспект Володимира Івасюка",
+            building = "54"
+        };
+
+        address.coordinates = new Coordinates();
+
+        // Act
+
+        await _coordinatesProvider.GetCoordinatesForAddress(address);
+
+        // Assert
+        Assert.Equal(50.52418025, address.coordinates.lat);
+        Assert.Equal(30.51606932744258, address.coordinates.lon);
+    }
+}
---
+++ ZAK/ZAK/Program.cs
@@ -0,0 +1,80 @@
+using ApplicationsScrappingModule;
+using BlazorApp;
+using BlazorApp.ApplicationsLoader;
+using BlazorApp.DA;
+using BlazorApp.DB;
+using BlazorApp.GeoDataManager;
+using Microsoft.EntityFrameworkCore;
+using ZAK.Components;
+
+namespace ZAK;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddBlazorBootstrap();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+
+
+    builder.Services.AddScoped<ICoefficientsDataAccess, CoefficientsDataAccess>();
+    builder.Services.AddScoped<IApplicationsDataAccess, ApplicationsDataAccess>();
+    builder.Services.AddScoped<IDistrictDataAccess, DistrictDataAccess>();
+    builder.Services.AddScoped<IBrigadesDataAccess, BrigadesDataAccess>();
+
+
+    builder.Services.AddScoped<IFileLoader, FileLoader>();
+    builder.Services.AddScoped<IApplicationsScrapper, ApplicationsScrapperUpdated>();
+    builder.Services.AddScoped<IGeoDataManager, GeoDataManager>();
+    builder.Services.AddScoped<IApplicationsLoader, ApplicationsLoader>();
+    builder.Services.AddScoped<IAddressesDataAccess, AddressesDataAccess>();
+    builder.Services.AddScoped<IAddressPriorityDataAccess, AddressPriorityDataAccess>();
+    builder.Services.AddScoped<IAddressAliasDataAccess, AddressAliasDataAccess>();
+    builder.Services.AddScoped<ICoordinatesDataAccess, CoordinatesDataAccess>();
+
+
+    builder.Services.AddScoped<IGeoDataManager, GeoDataManager>();
+
+    builder.Services.AddDbContext<BlazorAppDbContext>(
+    options =>
+    {
+        options.UseSqlite(@"Data Source=D:\C#_Projects\ZAK\ZAK2\BlazorApp.DB\DbFiles\testdb2.db");
+        //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+);
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.Run();
+    }
+}
