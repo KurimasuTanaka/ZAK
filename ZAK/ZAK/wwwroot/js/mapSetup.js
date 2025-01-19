68a6d1a5c8b34264760ff47b51d2c4c7711a6835
Kurimasu Tanaka <kamyshek185@gmail.com>
2025-01-19 12:29:55 +0200

    Basic markers added

--- ZAK/ZAK/wwwroot/js/mapSetup.js
+++ ZAK/ZAK/wwwroot/js/mapSetup.js
@@ -2,6 +2,7 @@
 var map;
 
 var polylineGroup;
+var markerGroup;
 
 function initMap() {
     map = L.map('map').setView([50.4503422102082, 30.524112747535554], 13);
@@ -11,6 +12,7 @@ function initMap() {
     }).addTo(map);
 
     polylineGroup = L.layerGroup().addTo(map);
+    markerGroup = L.layerGroup().addTo(map);
 }
 
 function drawPath(coordinates, color) {
@@ -22,13 +24,51 @@ function drawPath(coordinates, color) {
         cords.push([parseFloat(coordinates.split(' ')[i]), parseFloat(coordinates.split(' ')[i + 1])]);
     }
 
-    const polyline = L.polyline(cords, 
-        { 
-            color: color, 
+    const polyline = L.polyline(cords,
+        {
+            color: color,
             weight: 6,
             opacity: 0.7,
         });
     polylineGroup.addLayer(polyline);
+}
+
+
+
+function drawMarker(markerString) {
+
+    let markerData = markerString.split(' ');
+
+    let stretched = markerData[3];
+    let important = markerData[4];
+
+    let classToUse = "";
+
+    if (stretched == "Stretched") {
+        if (important == "True")
+            classToUse = "div-icon-imp-str";
+        else
+            classToUse = "div-icon-reg-str";
+    } else {
+        if (important == "True")
+            classToUse = "div-icon-imp-nstr";
+        else
+            classToUse = "div-icon-reg-nstr";
+
+    }
+
+    const marker = L.marker(
+        [parseFloat(markerData[0]), parseFloat(markerData[1])],
+        {
+            icon: L.divIcon({
+                className: classToUse
+            }
+            )
+        }
+
+    ).addTo(map);
+    marker.bindPopup(markerData[2]);
+    //markerGroup.addLayer(marker);
 }
 
 function clearMap() {
