c1ba525a03f5827ee19d5771d56474f652373e77
Kurimasu Tanaka <kamyshek185@gmail.com>
2025-05-11 07:33:31 +0300

    Schedule manager fully tested

--- ZAK.Tests/ScheduleManagerTests.cs
+++ ZAK.Tests/ScheduleManagerTests.cs
@@ -17,20 +17,15 @@ public class ScheduleManagerTests : ZakTestBase
     public async void InsertNewApplicationToEmptySchedule()
     {
         //Arrange
+        ScheduleManager scheduleManager = new(brigadesDao, scheduleManagerLogger);
 
-        //Creating brigade and adding it to the Db
         Brigade emptyBrigade = new();
         await brigadesDao.Insert(emptyBrigade);
 
-        ScheduleManager scheduleManager = new(brigadesDao, scheduleManagerLogger);
 
         //Adding new application to the Db
-        Address newAddress = new();
-        newAddress.streetName = "PaperStreet";
-        newAddress.building = "building";
 
         Application newApplication = new();
-        newApplication.address = newAddress;
 
         await applicationsDao.Insert(newApplication);
 
@@ -57,30 +52,68 @@ public class ScheduleManagerTests : ZakTestBase
     public async void InsertNewApplicationToScheduleBeforePreviouslyScheduledApplication()
     {
         //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
 
-        //Creating brigade and adding it to the Db
         Brigade emptyBrigade = new();
         await brigadesDao.Insert(emptyBrigade);
 
-        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
-
-        //Adding new application to the Db
-        Address newAddress = new();
-        newAddress.streetName = "PaperStreet 1";
-        newAddress.building = "building";
-
         Application newApplication1 = new();
-        newApplication1.address = newAddress;
+        newApplication1.operatorComment = "First application";
 
         Application newApplication2 = new();
-        newApplication1.address = newAddress;
-
-        Application newApplication3 = new();
-        newApplication1.address = newAddress;
+        newApplication2.operatorComment = "Second application";
 
         await applicationsDao.Insert(newApplication1);
         await applicationsDao.Insert(newApplication2);
-        await applicationsDao.Insert(newApplication3);
+
+        int timeToScheduleFirstApplication = 3;
+        int timeToScheduleSecondApplication = 2;
+
+        //Act 
+
+        Application firstApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
+        Application secondApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(1);
+
+        Brigade brigadeToEdit = (await brigadesDao.GetAll()).First();
+
+        await brigadesManager.ScheduleApplication(firstApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleFirstApplication);
+
+        await brigadesManager.ScheduleApplication(
+            secondApplicationToAddToSchedule.id,
+            brigadeToEdit.id,
+            timeToScheduleSecondApplication);
+
+        //Assert
+
+        Brigade editedBrigade = (await brigadesDao.GetAll(
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
+        )).First();
+
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Equal(secondApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[0].application.id);
+        Assert.Equal(firstApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[1].application.id);
+
+        Assert.Equal(timeToScheduleSecondApplication, editedBrigade.scheduledApplications[0].scheduledTime);
+        Assert.Equal(timeToScheduleFirstApplication, editedBrigade.scheduledApplications[1].scheduledTime);
+    }
+    [Fact]
+    public async void InsertNewApplicationToScheduleAfterPreviouslyScheduledApplication()
+    {
+        //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
+
+        Brigade emptyBrigade = new();
+        await brigadesDao.Insert(emptyBrigade);
+
+        Application newApplication1 = new();
+        newApplication1.operatorComment = "First application";
+
+        Application newApplication2 = new();
+        newApplication2.operatorComment = "Second application";
+
+        await applicationsDao.Insert(newApplication1);
+        await applicationsDao.Insert(newApplication2);
 
         int timeToScheduleFirstApplication = 3;
         int timeToScheduleSecondApplication = 4;
@@ -89,23 +122,271 @@ public class ScheduleManagerTests : ZakTestBase
 
         Application firstApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
         Application secondApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(1);
-        Application thirdApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(2);
 
         Brigade brigadeToEdit = (await brigadesDao.GetAll()).First();
 
         await brigadesManager.ScheduleApplication(firstApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleFirstApplication);
-        await brigadesManager.ScheduleApplication(secondApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleSecondApplication);
 
-        await brigadesManager.MoveScheduledApplicationFromOneBrigadeToAnother(thirdApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleFirstApplication, brigadeToEdit.id, 9);
+        await brigadesManager.ScheduleApplication(
+            secondApplicationToAddToSchedule.id,
+            brigadeToEdit.id,
+            timeToScheduleSecondApplication);
 
         //Assert
 
         Brigade editedBrigade = (await brigadesDao.GetAll(
-            query: b => b.Include(b => b.scheduledApplications)
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
         )).First();
 
-        Assert.Equal<int>(timeToScheduleFirstApplication, editedBrigade.GetApplicationScheduledOn(3).applicationScheduledTime);
-        Assert.Equal<int>(timeToScheduleSecondApplication, editedBrigade.GetApplicationScheduledOn(4).applicationScheduledTime);
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Equal(firstApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[0].application.id);
+        Assert.Equal(secondApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[1].application.id);
+
+        Assert.Equal(timeToScheduleFirstApplication, editedBrigade.scheduledApplications[0].scheduledTime);
+        Assert.Equal(timeToScheduleSecondApplication, editedBrigade.scheduledApplications[1].scheduledTime);
+
+    }
+
+    [Fact]
+    public async void ScheduleApplicationOnTimeWhereApplicationAlreadyExist()
+    {
+        //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
+
+        Brigade emptyBrigade = new();
+        await brigadesDao.Insert(emptyBrigade);
+
+        Application newApplication1 = new();
+        newApplication1.operatorComment = "First application";
+
+        Application newApplication2 = new();
+        newApplication2.operatorComment = "Second application";
+
+        await applicationsDao.Insert(newApplication1);
+        await applicationsDao.Insert(newApplication2);
+
+        int timeToScheduleApplication = 3;
+
+        //Act 
+
+        Application firstApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
+        Application secondApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(1);
+
+        Brigade brigadeToEdit = (await brigadesDao.GetAll()).First();
+
+        await brigadesManager.ScheduleApplication(firstApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleApplication);
+
+        await brigadesManager.ScheduleApplication(
+            secondApplicationToAddToSchedule.id,
+            brigadeToEdit.id,
+            timeToScheduleApplication);
+
+        //Assert
+
+        Brigade editedBrigade = (await brigadesDao.GetAll(
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
+        )).First();
+
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Single(editedBrigade.scheduledApplications);
+        Assert.Equal(secondApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[0].applicationId);
+
+    }
+
+    [Fact]
+    public async void MoveScheduledApplicationFromOneBrigadeToAnother()
+    {
+        //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
+
+        Brigade brigade1 = new();
+        Brigade brigade2 = new();
+
+        await brigadesDao.Insert(brigade1);
+        await brigadesDao.Insert(brigade2);
+
+        Application newApplication1 = new();
+        newApplication1.operatorComment = "First application";
+
+        Application newApplication2 = new();
+        newApplication2.operatorComment = "Second application";
+
+        Application newApplication3 = new();
+        newApplication3.operatorComment = "Third application";
+
+        Application newApplication4 = new();
+        newApplication3.operatorComment = "Forth application";
+
+
+        await applicationsDao.Insert(newApplication1);
+        await applicationsDao.Insert(newApplication2);
+        await applicationsDao.Insert(newApplication3);
+        await applicationsDao.Insert(newApplication4);
+
+        int timeToScheduleFirstApplication = 1;
+        int timeToScheduleSecondApplication = 9;
+        int timeToScheduleThirdApplication = 5;
+        int timeToScheduleForthApplication = 7;
+
+
+        Application firstApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
+        Application secondApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(1);
+        Application thirdApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(2);
+        Application forthApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(2);
+
+        Brigade brigadeToEdit1 = (await brigadesDao.GetAll()).ElementAt(0);
+        Brigade brigadeToEdit2 = (await brigadesDao.GetAll()).ElementAt(1);
+
+        await brigadesManager.ScheduleApplication(firstApplicationToAddToSchedule.id, brigadeToEdit2.id, timeToScheduleFirstApplication);
+        await brigadesManager.ScheduleApplication(secondApplicationToAddToSchedule.id, brigadeToEdit2.id, timeToScheduleSecondApplication);
+        await brigadesManager.ScheduleApplication(forthApplicationToAddToSchedule.id, brigadeToEdit2.id, timeToScheduleForthApplication);
+        await brigadesManager.ScheduleApplication(thirdApplicationToAddToSchedule.id, brigadeToEdit1.id, timeToScheduleThirdApplication);
+
+        //Act 
+
+        await brigadesManager.MoveScheduledApplicationFromOneBrigadeToAnother(
+            thirdApplicationToAddToSchedule.id,
+            brigadeToEdit2.id,
+            timeToScheduleThirdApplication,
+            brigadeToEdit1.id,
+            timeToScheduleThirdApplication);
+
+        //Assert
+
+        Brigade editedBrigade = (await brigadesDao.GetAll(
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
+        )).ElementAt(1);
+
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Equal(3, editedBrigade.scheduledApplications.Count);
+        Assert.Equal(firstApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[0].applicationId);
+        Assert.Equal(thirdApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[1].applicationId);
+
+        Assert.Equal(forthApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[2].applicationId);
+        Assert.Equal(timeToScheduleForthApplication + 1, editedBrigade.scheduledApplications[2].scheduledTime);
+
+    }
+
+    [Fact]
+    public async void DeleteScheduledApplicationFromSchedule()
+    {
+        //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
+
+        Brigade emptyBrigade = new();
+        await brigadesDao.Insert(emptyBrigade);
+
+        Application newApplication = new();
+        newApplication.operatorComment = "First application";
+
+        await applicationsDao.Insert(newApplication);
+
+        int timeToScheduleApplication = 3;
+
+        //Act 
+
+        Application applicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
+
+        Brigade brigadeToEdit = (await brigadesDao.GetAll()).First();
+
+        await brigadesManager.ScheduleApplication(applicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleApplication);
+
+        await brigadesManager.MakeTimeSlotEmpty(
+            brigadeToEdit.id,
+            timeToScheduleApplication);
+
+        //Assert
+
+        Brigade editedBrigade = (await brigadesDao.GetAll(
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
+        )).First();
+
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Empty(editedBrigade.scheduledApplications);
+    }
+
+    [Fact]
+    public async void MoveScheduledApplicationFromOneTimeToAnother()
+    {
+        //Arrange
+        ScheduleManager brigadesManager = new(brigadesDao, scheduleManagerLogger);
+
+        Brigade brigade = new();
+
+        await brigadesDao.Insert(brigade);
+
+        Application newApplication1 = new();
+        newApplication1.operatorComment = "First application";
+
+        Application newApplication2 = new();
+        newApplication2.operatorComment = "Second application";
+
+        Application newApplication3 = new();
+        newApplication3.operatorComment = "Third application";
+
+        Application newApplication4 = new();
+        newApplication4.operatorComment = "Forth application";
+
+
+        await applicationsDao.Insert(newApplication1);
+        await applicationsDao.Insert(newApplication2);
+        await applicationsDao.Insert(newApplication3);
+        await applicationsDao.Insert(newApplication4);
+
+        int timeToScheduleFirstApplication = 1;
+        int timeToScheduleSecondApplication = 2;
+        int timeToScheduleThirdApplication = 5;
+        int timeToScheduleForthApplication = 9;
+
+        int timeToMoveApplication = 7;
+
+        Application firstApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(0);
+        Application secondApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(1);
+        Application thirdApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(2);
+        Application forthApplicationToAddToSchedule = (await applicationsDao.GetAll()).ElementAt(3);
+
+        Brigade brigadeToEdit = (await brigadesDao.GetAll()).ElementAt(0);
+
+        await brigadesManager.ScheduleApplication(firstApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleFirstApplication);
+        await brigadesManager.ScheduleApplication(secondApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleSecondApplication);
+        await brigadesManager.ScheduleApplication(thirdApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleThirdApplication);
+        await brigadesManager.ScheduleApplication(forthApplicationToAddToSchedule.id, brigadeToEdit.id, timeToScheduleForthApplication);
+
+        //Act 
+
+        await brigadesManager.MoveScheduledApplicationFromOneBrigadeToAnother(
+            secondApplicationToAddToSchedule.id,
+            brigadeToEdit.id,
+            timeToMoveApplication,
+            brigadeToEdit.id,
+            timeToScheduleSecondApplication);
+
+        //Assert
+
+        Brigade editedBrigade = (await brigadesDao.GetAll(
+            query: b => b.Include(b => b.scheduledApplications).ThenInclude(sa => sa.application)
+        )).ElementAt(0);
+
+        editedBrigade.scheduledApplications.OrderBy(sa => sa.scheduledTime);
+
+        Assert.Equal(4, editedBrigade.scheduledApplications.Count);
+
+        Assert.Equal(firstApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[0].applicationId);
+        Assert.Equal(thirdApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[1].applicationId);
+        Assert.Equal(secondApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[2].applicationId);
+        Assert.Equal(forthApplicationToAddToSchedule.id, editedBrigade.scheduledApplications[3].applicationId);
+
+        Assert.Equal(timeToScheduleFirstApplication, editedBrigade.scheduledApplications[0].scheduledTime);
+        Assert.Equal(timeToScheduleThirdApplication - 1, editedBrigade.scheduledApplications[1].scheduledTime);
+        Assert.Equal(timeToMoveApplication, editedBrigade.scheduledApplications[2].scheduledTime);
+        Assert.Equal(timeToScheduleForthApplication, editedBrigade.scheduledApplications[3].scheduledTime);
+
+
+
     }
 
 }
--- ZAK/ZAK/Services/ScheduleManagerService/ScheduleManager.cs
+++ ZAK/ZAK/Services/ScheduleManagerService/ScheduleManager.cs
@@ -16,9 +16,9 @@ public class ScheduleManager : IScheduleManager
         _logger = logger;
     }
 
-    public async Task MoveScheduledApplicationFromOneBrigadeToAnother(int applicationId, int brigadeId, int time, int prevBrigadeId, int prevTime)
+    public async Task MoveScheduledApplicationFromOneBrigadeToAnother(int applicationId, int brigadeId, int newTime, int prevBrigadeId, int prevTime)
     {
-        _logger.LogInformation($"Inserting application {applicationId} to brigade {brigadeId} at {time} hour");
+        _logger.LogInformation($"Inserting application {applicationId} to brigade {brigadeId} at {newTime} hour");
         _logger.LogInformation($"Deleting application {applicationId} from previous brigade {prevBrigadeId}...");
 
         //Delete application from previous place
@@ -33,18 +33,24 @@ public class ScheduleManager : IScheduleManager
         _logger.LogInformation($"Moving applications in brigade {prevBrigadeId} that scheduled after {prevTime} hour to the previous hour...");
 
         //Move all applications that scheduled after new one to the previous hour
-        for (int i = 0; i < prevBrigade.scheduledApplications.Count; i++)
-        {
-            if (prevBrigade.scheduledApplications[i].scheduledTime >= prevTime)
-            {
-                //Delete application from previous scheduled time
-                ScheduledApplicationModel scheduledApplication = prevBrigade.scheduledApplications[i];
-                scheduledApplication.scheduledTime--;
+        // for (int i = 0; i < prevBrigade.scheduledApplications.Count; i++)
+        // {
+        //     if (prevBrigade.scheduledApplications[i].scheduledTime >= prevTime)
+        //     {
+        //         //Delete application from previous scheduled time
+        //         ScheduledApplicationModel scheduledApplication = prevBrigade.scheduledApplications[i];
+        //         scheduledApplication.scheduledTime--;
 
-                prevBrigade.scheduledApplications.RemoveAll(sa => sa.scheduledTime == prevBrigade.scheduledApplications[i].scheduledTime);
-                prevBrigade.scheduledApplications.Add(scheduledApplication);
-            }
-        }
+        //         prevBrigade.scheduledApplications.RemoveAll(sa => sa.scheduledTime == prevBrigade.scheduledApplications[i].scheduledTime);
+        //         prevBrigade.scheduledApplications.Add(scheduledApplication);
+        //     }
+        // }
+
+        prevBrigade.scheduledApplications.RemoveAll(sa => sa.scheduledTime == prevTime);
+        prevBrigade.scheduledApplications.Where(sa => sa.scheduledTime > prevTime).ToList().ForEach(sa =>
+        {
+            sa.scheduledTime--;
+        });
 
         _logger.LogInformation($"Updating previous brigade {prevBrigadeId}...");
         //Update previous brigade
@@ -82,36 +88,31 @@ public class ScheduleManager : IScheduleManager
             newBrigade.brigadeSlotsCount++;
         }
 
-        _logger.LogInformation($"Moving applications in brigade {brigadeId} that scheduled after {time} hour to the next hour...");
+        _logger.LogInformation($"Moving applications in brigade {brigadeId} that scheduled after {newTime} hour to the next hour...");
+
         //Move all applications that scheduled after new one to the next hour
-
-        for (int i = 0; i < newBrigade.scheduledApplications.Count; i++)
+        newBrigade.scheduledApplications.Where(sa => sa.scheduledTime > newTime).ToList().ForEach(sa =>
         {
-            if (newBrigade.scheduledApplications[i].scheduledTime >= time)
-            {
-                //Delete application from previous scheduled time
-                ScheduledApplicationModel scheduledApplication = newBrigade.scheduledApplications[i];
-                scheduledApplication.scheduledTime++;
-
-                newBrigade.scheduledApplications.RemoveAll(sa => sa.scheduledTime == newBrigade.scheduledApplications[i].scheduledTime);
-                newBrigade.scheduledApplications.Add(scheduledApplication);
-            }
-        }
+            sa.scheduledTime++;
+        });
+        newBrigade.scheduledApplications.RemoveAll(sa => sa.scheduledTime > 9);
 
         if (applicationId != 0)
         {
-            _logger.LogInformation($"Inserting application {applicationId} in brigade {brigadeId} on time {time}...");
+            _logger.LogInformation($"Inserting application {applicationId} in brigade {brigadeId} on time {newTime}...");
             //Insert new application to the schedule
             ScheduledApplicationModel newScheduledApplication = new ScheduledApplicationModel()
             {
                 applicationId = applicationId,
                 brigadeId = brigadeId,
-                scheduledTime = time
+                scheduledTime = newTime
             };
             newBrigade.scheduledApplications.Add(newScheduledApplication);
         }
-        _logger.LogInformation($"Updating new brigade {brigadeId}...");
 
+
+
+        _logger.LogInformation($"Updating new brigade {brigadeId}...");
         //Update new brigade
         await _brigadeDataAccess.Update(
         newBrigade,
@@ -159,11 +160,21 @@ public class ScheduleManager : IScheduleManager
         findPredicate: b => b.id == newBrigade.id,
         inputDataProccessingQuery: (b, context) =>
         {
-            foreach (var schedule in newBrigade.scheduledApplications)
+            foreach (var scheduledApplication in newBrigade.scheduledApplications)
             {
-                if (schedule.application != null)
+                if (scheduledApplication.applicationId != applicationId && scheduledApplication.scheduledTime == time)
                 {
-                    context.Attach(schedule.application);
+                    b.scheduledApplications.Remove(scheduledApplication);
+                    break;
+                }
+            }
+
+
+            foreach (var scheduledApplication in newBrigade.scheduledApplications)
+            {
+                if (scheduledApplication.application != null)
+                {
+                    context.Attach(scheduledApplication.application);
                 }
             }
             return b;
@@ -180,7 +191,7 @@ public class ScheduleManager : IScheduleManager
         if (brigade is null) throw new Exception("New brigade not found");
 
         ScheduledApplicationModel? scheduledApplication = brigade.scheduledApplications.Where(sa => sa.scheduledTime == time).First();
-        if(scheduledApplication is null) return;
+        if (scheduledApplication is null) return;
 
         brigade.scheduledApplications.Remove(scheduledApplication);
 
