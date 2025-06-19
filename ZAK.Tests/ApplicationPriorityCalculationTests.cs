f30e09101681574b6920d43b33e23c15ab16dc43
Kurimasu Tanaka <kamyshek185@gmail.com>
2025-06-19 09:53:45 +0300

    Application priority calculation test added

---
+++ ZAK.Tests/ApplicationPriorityCalculationTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using ZAK.DA;
+using ZAK.Db.Models;
+
+namespace ZAK.Tests;
+
+public class ApplicationPriorityCalculationTests
+{
+    [Fact]
+    public void CalculatePriorityByDistance()
+    {
+        // Arrange
+        Application application1 = new();
+        Application application2 = new();
+        Application application3 = new();
+
+        application1.address = new AddressModel();
+        application1.address.coordinates = new AddressCoordinatesModel { lat = 10, lon = 10 };
+
+        application2.address = new AddressModel();
+        application2.address.coordinates = new AddressCoordinatesModel { lat = 20, lon = 10 };
+
+        application3.address = new AddressModel();
+        application3.address.coordinates = new AddressCoordinatesModel { lat = 40, lon = 10 };
+
+        Dictionary<string, double> coefficients = new()
+        {
+            { "distance", 10 },
+            { "housePriority", 1 },
+            { "urgency", 1 },
+            { "statusCheck", 1 },
+            { "freeCable", 1 },
+            { "tarrifeChangeApplication", 1 },
+            { "deadline", 1 }
+        };
+
+        // Act
+        application2.CalculateApplicationPriorityLevel(
+            coefficients,
+            new List<Application> { application1}
+        );
+
+        double application2PriorityNearApp1 = application2.priority;
+
+        application2.CalculateApplicationPriorityLevel(
+            coefficients,
+            new List<Application> { application3}
+        );
+
+        double application2PriorityNearApp3 = application2.priority;
+
+        // Assert
+        Assert.True(application2PriorityNearApp1 > application2PriorityNearApp3, 
+            "Application priority should be higher when closer to another application.");
+    }
+}
